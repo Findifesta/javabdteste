f3a5588011fd0a66ee18fe0c27faa0a158ca4824
Sérgio da Rocha <113067613+Findifesta@users.noreply.github.com>
2024-06-07 20:14:11 -0300

    Update Aulabd2Application.java

--- src/main/java/com/aulabd2/aulabd2/Aulabd2Application.java
+++ src/main/java/com/aulabd2/aulabd2/Aulabd2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Aulabd2Application {
+public class ZapLivrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Aulabd2Application.class, args);
+		SpringApplication.run(ZapLivrosApplication.class, args);
 	}
 
 }
