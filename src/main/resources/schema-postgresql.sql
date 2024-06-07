1b4a6e97acf547f5a0a415f641ad3cafd0fcbc4c
Sérgio da Rocha <113067613+Findifesta@users.noreply.github.com>
2024-06-07 20:15:22 -0300

    Update schema-postgresql.sql

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,5 +1,9 @@
-CREATE TABLE IF NOT EXISTS cliente (
+CREATE TABLE IF NOT EXISTS funcionario (
      id serial PRIMARY KEY,
      nome  varchar(50),
-     cpf   varchar(11)
+     cpf   varchar(11),
+     email   varchar(50),
+     telefone   varchar(11),
+     senha varchar(50),
+     cargo boolean
 );
